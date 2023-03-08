fcc0c086fcd79e5f3f41e08e991fd1e4be17de6c
medvik <kabiamohamed1997@gmail.com>
2023-03-08 15:26:44 +0000

    add invoice page

---
+++ pages/invoice/_id.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Titlebar title="Fee Name"/>
+        <v-container>
+           {{ id }}
+        </v-container>
+    </div>
+</template>
+<script>
+     import Titlebar from "../../components/default/Titlebar.vue";
+export default {
+    components:{Titlebar},
+       
+    computed: {
+        id(){
+            return this.$route.params.id
+        }
+    },
+}
+</script>
---
+++ pages/invoice/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <v-container>
+            <Titlebar title="Invoice"/>
+            <Invoice/>
+        </v-container>
+    </div>
+</template>
+<script>
+    import Invoice from '../../components/Invoice.vue'
+    import Titlebar from "../../components/default/Titlebar.vue";
+export default {
+    name:'StudentList',
+    components:{Invoice,Titlebar},
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
